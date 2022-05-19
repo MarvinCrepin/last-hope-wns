97db9a77e78507432c5af3dbf96d7a72033e4ec6
florian <florian.beme@victeon.fr>
2022-05-19 14:14:45 +0200

    ajout composant dashboard

--- front/src/components/Dashboard/TaskList.tsx
+++ front/src/components/Dashboard/TaskList.tsx
@@ -3,5 +3,33 @@ import React from "react";
 type Props = {};
 
 export default function TaskList({}: Props) {
-  return <div>TaskList</div>;
+  return (
+    <div>
+      <div className="w-full bg-lh-primary z-20 py-8 px-2 rounded-tr-md">
+        <div className="flex items-center space-x-4">
+          <label className="sr-only" htmlFor="filterSelect">
+            Filter:
+          </label>
+
+          <select
+            name="filterSelect"
+            id="filterSelect"
+            className="w-36 rounded-md bg-lh-secondary text-lh-light p-2"
+          >
+            <option value="allProject">All Projects</option>
+          </select>
+          <div className="">
+            <input type="checkbox" name="" id="" />{" "}
+            <span className="text-lh-light"> Assigned to me only</span>
+          </div>
+          <div>
+            <input type="checkbox" name="" id="" />
+            <span className="text-lh-light"> Hide done</span>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div>tab</div>
+    </div>
+  );
 }
