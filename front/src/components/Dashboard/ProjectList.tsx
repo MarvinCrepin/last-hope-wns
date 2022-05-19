cd5a5ece143ed3c128b5fd7e712f60c9544dac29
florian <florian.beme@victeon.fr>
2022-05-19 15:26:39 +0200

    suppression code commenté

--- front/src/components/Dashboard/ProjectList.tsx
+++ front/src/components/Dashboard/ProjectList.tsx
@@ -34,37 +34,6 @@ export default function ProjectList() {
           </div>
         </div>
       </div>
-      {/* <div className="w-full bg-lh-primary z-20 py-8 px-2 rounded-tr-md md:h-30">
-        <div className="flex flex-col space-y-5 md:space-y-0 md:flex-row justify-between items-center font-text ">
-          <div className="flex items-center">
-            <div className="mx-2 flex items-center space-x-1">
-              <input
-                className="rounded-md h-5 w-5"
-                type="checkbox"
-                name="hideDone"
-                id="hideDone"
-              />
-              <label htmlFor="hideDone" className="text-lh-light">
-                Hide done
-              </label>
-            </div>
-          </div>
-
-          <div className="relative flex item-centers">
-            <label htmlFor="searchInput" className="sr-only">
-              Recherche
-            </label>
-            <input
-              type="text"
-              id="searchInput"
-              name="searchInput"
-              placeholder="Search"
-              className="rounded-md h-8 mx-2 px-8"
-            />
-            <FaSearch className="absolute top-2 left-4 text-gray-500" />
-          </div>
-        </div>
-      </div> */}
       <div>Project list</div>
     </div>
   );
