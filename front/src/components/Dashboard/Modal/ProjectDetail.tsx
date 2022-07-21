618cdbc13ae547d687691284ab6bee47be61b770
Davy M <87863422+iStyyx@users.noreply.github.com>
2022-07-21 09:45:56 +0200

    Some changes on the project detail

--- front/src/components/Dashboard/Modal/ProjectDetail.tsx
+++ front/src/components/Dashboard/Modal/ProjectDetail.tsx
@@ -1,46 +1,53 @@
 import { FaCalendarDay, FaCalendarCheck } from "react-icons/fa";
 import { MdOutlineAccountCircle } from "react-icons/md";
 import Moment from "react-moment";
-import { Line } from 'react-chartjs-2';
-import { Chart, registerables } from 'chart.js';
+import { Line } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
 import "../../../assets/css/projectDetail.css";
 import { Participant, Project } from "../../global";
+import { useSelector } from "react-redux";
+import { role } from "../../../slicer/authSlice";
+
+
 Chart.register(...registerables);
 
 const projectStats = {
-  labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi','Vendredi', 'Samedi'],
-  datasets: [{
-    label: 'Project Statistics (tasks/day)',
-    data: [4, 1, 3, 3, 6, 2],
-    fill: false,
-    borderColor: 'rgb(214 96 96)',
-    backgroundColor: 'rgb(214 96 96)',
-    pointRadius: 5,
-    pointHoverRadius: 10,
-    pointHitRadius: 30,
-    pointBorderWidth: 2,  
-  }]
+  labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+  datasets: [
+    {
+      label: "Project Statistics (tasks/day)",
+      data: [4, 1, 3, 3, 6, 2],
+      fill: false,
+      borderColor: "rgb(214 96 96)",
+      backgroundColor: "rgb(214 96 96)",
+      pointRadius: 5,
+      pointHoverRadius: 10,
+      pointHitRadius: 30,
+      pointBorderWidth: 2,
+    },
+  ],
 };
 
 const memberStats = {
-  labels: ['Lundi', 'Mardi', 'Mercredi', 'Jeudi','Vendredi', 'Samedi'],
-  datasets: [{
-    label: 'Project Statistics (hours/day)',
-    data: [3, 5, 5, 4, 7, 1, 0],
-    fill: false,
-    borderColor: 'rgb(214 96 96)',
-    backgroundColor: 'rgb(214 96 96)',
-    pointRadius: 5,
-    pointHoverRadius: 10,
-    pointHitRadius: 30,
-    pointBorderWidth: 2,  
-  }]
+  labels: ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"],
+  datasets: [
+    {
+      label: "Project Statistics (hours/day)",
+      data: [3, 5, 5, 4, 7, 1, 0],
+      fill: false,
+      borderColor: "rgb(214 96 96)",
+      backgroundColor: "rgb(214 96 96)",
+      pointRadius: 5,
+      pointHoverRadius: 10,
+      pointHitRadius: 30,
+      pointBorderWidth: 2,
+    },
+  ],
 };
 
 const options: any = {
   maintaintAspectRatio: false,
 };
-
 
 type Props = {
   project: Project;
@@ -48,6 +55,8 @@ type Props = {
 };
 
 function ProjectDetail({ project, closeModal }: Props) {
+  const userRole = useSelector(role);
+
   return (
     <div
       className="fixed z-10 inset-0 overflow-y-auto"
@@ -70,57 +79,122 @@ function ProjectDetail({ project, closeModal }: Props) {
         <div className="inline-block align-bottom text-left transform transition-all sm:align-middle project-modal">
           <div className=" bg-white  rounded-lg  flex justify-center min-h-full p-4 text-center sm:p-0">
             <div className=" relative bg-white rounded-lg text-left overflow-hidden transform transition-all project-modal-inside">
-              <div className="project-modal-infos py-8 px-2 sm:pl-6 sm:pr-10">
+              <div className="project-modal-infos py-8 px-2 sm:pl-6 sm:pr-6">
                 <div>
-                  <h2 className="text-4xl font-title text-lh-primary ">{"{ " + project.title + " }"}</h2>
-                  {(project.description) === "null" ? (<p className="pt-4 pb-6">No description defined</p>) : (
-                  <p className="text-gray-700 pt-4 pb-6 text-xl">
-                   {project.description}
-                  </p>)
-                  }
+                  <h2 className="text-4xl font-title text-lh-primary ">
+                    {"{ " + project.title + " }"}
+                  </h2>
+                  {project.description === "null" ? (
+                    <p className="pt-4 pb-6">No description defined</p>
+                  ) : (
+                    <p className="text-gray-700 pt-4 pb-6 text-xl">
+                      {project.description}
+                    </p>
+                  )}
                 </div>
                 <div>
-                  <h2 className="text-4xl font-title text-lh-primary ">Project Owner</h2>
+                  <h2 className="text-4xl font-title text-lh-primary ">
+                    Project Owner
+                  </h2>
+                  {userRole === ""}
                   <div className="pt-4 pb-6 flex items-center">
-                  {
-                  (project.product_owner) ?
-                    (<><MdOutlineAccountCircle size={30} /><p className="text-gray-700 font-title pl-3 text-xl">{project.product_owner.firstname + ' ' + project.product_owner.lastname}</p></>)
-                    : 'No project manager defined'
-                  }
-                    </div>
+                    {project.product_owner ? (
+                      <>
+                        <MdOutlineAccountCircle size={30} />
+                        <p className="text-gray-700 font-title pl-3 text-xl">
+                          {project.product_owner.firstname +
+                            " " +
+                            project.product_owner.lastname}
+                        </p>
+                      </>
+                    ) : (
+                      "No project manager defined"
+                    )}
+                  </div>
                 </div>
                 <div>
-                  <h2 className="text-4xl font-title text-lh-primary ">Members</h2>
+                  <h2 className="text-4xl font-title text-lh-primary ">
+                    Members
+                  </h2>
                   <ul className="list-disc list-inside pt-4 pb-6 flex flex-wrap">
-                    {(project.participants).length === 0 ? <p>No members defined</p> :
-                    
-                    project.participants.map((participant: Participant) => (
-                      <li key={participant.user.id} className="flex items-center py-2 pr-4">
-                        <MdOutlineAccountCircle size={30} /><p className="text-gray-700 font-title pl-3 text-xl">{participant.user.firstname} {participant.user.lastname} - <span className="text-lh-light-gray">{participant.user.roles}</span></p>
-                      </li>
-                    ))}
+                    {project.participants.length === 0 ? (
+                      <p>No members defined</p>
+                    ) : (
+                      project.participants.map((participant: Participant) => (
+                        <li
+                          key={participant.user.id}
+                          className="flex items-center py-2 pr-4"
+                        >
+                          <MdOutlineAccountCircle size={30} />
+                          <p className="text-gray-700 font-title pl-3 text-xl">
+                            {participant.user.firstname}{" "}
+                            {participant.user.lastname} - 
+                            <span className="text-lh-light-gray">
+                              {(() => {
+                                switch (participant.user.roles) {
+                                  case "ROLE_DEVELOPER":
+                                    return " Developer";
+                                  case "ROLE_PROJECT_MANAGER":
+                                    return " Project Manager";
+                                  case "ROLE_ADMIN":
+                                    return " Admin";
+                                  default:
+                                    return "No role defined";
+                                }
+                              })()}
+                            </span>
+                          </p>
+                        </li>
+                      ))
+                    )}
                   </ul>
                 </div>
                 <div>
-                  <h2 className="text-4xl font-title text-lh-primary ">Infos</h2>
-                    <div className="pt-4 pb-6">
-                      <p className="text-gray-700 font-title text-xl flex"><FaCalendarDay /><span className="pl-3">Start date: <Moment format="MM/DD/YYYY">{(project.start_at) ? project.start_at.toString() : 'Not Defined'}</Moment></span></p> 
-                      <p className="text-gray-700 font-title text-xl flex pt-3"><FaCalendarCheck /><span className="pl-3">End day: <Moment format="MM/DD/YYYY">{(project.end_at) ? project.end_at.toString() : 'Not Defined'}</Moment></span></p>
-                    </div>
+                  <h2 className="text-4xl font-title text-lh-primary ">
+                    Infos
+                  </h2>
+                  <div className="pt-4 pb-6">
+                    <p className="text-gray-700 font-title text-xl flex">
+                      <FaCalendarDay />
+                      <span className="pl-3">
+                        Start date:{" "}
+                        <Moment format="MM/DD/YYYY">
+                          {project.start_at
+                            ? project.start_at.toString()
+                            : "Not Defined"}
+                        </Moment>
+                      </span>
+                    </p>
+                    <p className="text-gray-700 font-title text-xl flex pt-3">
+                      <FaCalendarCheck />
+                      <span className="pl-3">
+                        End day:{" "}
+                        <Moment format="MM/DD/YYYY">
+                          {project.end_at
+                            ? project.end_at.toString()
+                            : "Not Defined"}
+                        </Moment>
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
               <hr />
-              <div className="project-modal-stats py-8 px-2 sm:pl-6 sm:pr-10">
+              <div className="project-modal-stats py-8 px-2 sm:pl-6 sm:pr-4">
                 <div className="project-modal-pstats pb-10">
-                  <h2 className="text-4xl font-title text-lh-primary ">Project Statistics</h2>
+                  <h2 className="text-4xl font-title text-lh-primary ">
+                    Project Statistics
+                  </h2>
                   <div>
-                    <Line data={projectStats} options={options}/>
+                    <Line data={projectStats} options={options} />
                   </div>
                 </div>
                 <div className="project-modal-mstats">
-                  <h2 className="text-4xl font-title text-lh-primary ">Member Statistics</h2>
+                  <h2 className="text-4xl font-title text-lh-primary ">
+                    Member Statistics
+                  </h2>
                   <div>
-                    <Line data={memberStats} options={options}/>
+                    <Line data={memberStats} options={options} />
                   </div>
                 </div>
               </div>
