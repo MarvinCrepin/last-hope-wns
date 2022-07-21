a1e2599b8a3feca7214629e78d083d25a80a45d9
weisgerber <akumaony>
2022-07-21 11:08:25 +0200

    create notification create method

---
+++ api/src/helpers/createNotification.ts
@@ -0,0 +1,25 @@
+import { Context } from "../graphql/resolvers/types";
+
+export default async function createNotification(
+  title: string,
+  content: string,
+  type: string,
+  context: Context,
+  userId: string
+) {
+  if (title && content && type && userId && context) {
+    try {
+      const newNotification = await context.prisma.notification.create({
+        title,
+        content,
+        type,
+        is_read: false,
+        user_id: userId,
+      });
+
+      return newNotification;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
