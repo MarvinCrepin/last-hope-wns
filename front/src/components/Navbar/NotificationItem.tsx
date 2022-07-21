768f25567b46b16069654a986733001ffbe5747d
weisgerber <akumaony>
2022-07-21 09:11:03 +0200

    delete comment

--- front/src/components/Navbar/NotificationItem.tsx
+++ front/src/components/Navbar/NotificationItem.tsx
@@ -19,21 +19,6 @@ export default function NotificationItem({ notification }: IMyProps) {
           <div className="flex items-start">
             <div className="flex-shrink-0">
               {notificationIcone(notification.type, "h-6 w-6 text-lh-primary")}
-              {/* <svg
-                className="h-6 w-6 text-lh-primary"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                />
-              </svg> */}
             </div>
             <div className="ml-3 w-0 flex-1 pt-0.5">
               <p className="text-sm font-medium text-lh-primary">
