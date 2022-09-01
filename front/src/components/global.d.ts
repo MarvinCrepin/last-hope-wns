bac419b489c1368ac54ed54ae4421ac45188e5bf
florian <florian.beme@victeon.fr>
2022-09-01 17:24:15 +0200

    FIX | Front | fix Projet name dans tasklist

--- front/src/components/global.d.ts
+++ front/src/components/global.d.ts
@@ -59,6 +59,7 @@ export type TaskInList = {
   advancement: number;
   due_at: Date;
   participants: UserParticipant[];
+  project_name: string;
   estimated_time: number;
   description: string | null;
   state_id: string;
