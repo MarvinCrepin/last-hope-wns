40154130d377f635983018eb24c939aeff43ced5
weisgerber <akumaony>
2022-10-04 18:03:40 +0200

    added splash screen when fonts are loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,33 +10,39 @@ import tw from "./lib/tailwind";
 
 import * as Font from "expo-font";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Text } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
 const Tab = createBottomTabNavigator();
 
-const fetchFonts = () =>
-  Font.loadAsync({
-    "Baloo Da 2 Medium": require("./src/assets/fonts/BalooDa2-Medium.ttf"),
-    "Baloo Da 2 Regular": require("./src/assets/fonts/BalooDa2-Regular.ttf"),
-    "Baloo Da 2 SemiBold": require("./src/assets/fonts/BalooDa2-SemiBold.ttf"),
-    "Cabin Sketch": require("./src/assets/fonts/CabinSketch-Regular.ttf"),
-    "Cabin Sketch Bold": require("./src/assets/fonts/CabinSketch-Bold.ttf"),
-  });
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [appIsReady, setAppIsReady] = useState(false);
+
   useEffect(() => {
-    async function getFonts() {
-      await fetchFonts();
-      setFontsLoaded(true);
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          // "Baloo Da 2 Medium": require("./src/assets/fonts/BalooDa2-Medium.ttf"),
+          "Baloo Da 2 Regular": require("./src/assets/fonts/BalooDa2-Regular.ttf"),
+          // "Baloo Da 2 SemiBold": require("./src/assets/fonts/BalooDa2-SemiBold.ttf"),
+          "Cabin Sketch": require("./src/assets/fonts/CabinSketch-Regular.ttf"),
+          // "Cabin Sketch Bold": require("./src/assets/fonts/CabinSketch-Bold.ttf"),
+        });
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
     }
-    getFonts();
+
+    prepare();
   }, []);
 
-  if (!fontsLoaded) {
-    return <Text>Loading...</Text>;
+  if (!appIsReady) {
+    return null;
   }
-
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -51,7 +57,7 @@ export default function App() {
           tabBarShowLabel: false,
         })}
       >
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} initialParams={{appIsReady:appIsReady}}/>
         <Tab.Screen name="Account" component={AccountScreen} />
         <Tab.Screen name="Search" component={SearchScreen} />
       </Tab.Navigator>
--- mobile/src/screens/HomeScreen.tsx
+++ mobile/src/screens/HomeScreen.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { View, Text } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
 import tw from "../../lib/tailwind";
 
-export default function HomeScreen() {
+export default function HomeScreen({ route }: { route: any; navigation: any }) {
+  const { appIsReady } = route.params;
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
   return (
-    <View style={tw.style("bg-primary")}>
-      <Text style={tw.style( "font-text")}>Title</Text>
+    <View style={tw.style("bg-primary")} onLayout={onLayoutRootView}>
+      <Text style={tw.style("font-title")}>Title</Text>
     </View>
   );
 }
