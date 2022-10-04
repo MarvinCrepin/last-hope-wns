55e53a738848f2e5ea9bb58a4ba379767028d842
weisgerber <akumaony>
2022-10-04 11:16:57 +0200

    resize icons

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import SearchScreen from "./src/screens/SearchScreen";
@@ -12,7 +13,26 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = "home";
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Account") {
+              iconName = focused ? "person-circle" : "person-circle-outline";
+              size += 5;
+            } else if (route.name === "Search") {
+              iconName = focused ? "search-circle" : "search-circle-outline";
+              size += 8;
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+          tabBarShowLabel: false,
+        })}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Account" component={AccountScreen} />
         <Tab.Screen name="Search" component={SearchScreen} />
