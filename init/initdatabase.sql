f111e655194eb94d6cb1d21c54631c9b2a0ad50f
florian <florian.beme@victeon.fr>
2022-12-12 18:16:58 +0100

    change docker-compose

---
+++ init/initdatabase.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS `test`;
+INSERT INTO test.User (id, lastname, firstname, mail, roles, password) VALUES ('cl670ivfe00000ppe53igfyqj', 'Florian', 'Bême', 'florianbme@gmail.com', 'ROLE_ADMIN', '$2b$10$UoOcMJXNofjBtMwm6ag7Feu.NQisSH.T4482XNVRRQXKQ6/9e4MUq');
+
